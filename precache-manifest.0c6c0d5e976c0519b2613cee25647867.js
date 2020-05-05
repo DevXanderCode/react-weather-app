72e5fa87b911e447302ce0bbdd5f3ea95acf3135
Chinedu Alex <nwekealexchinedu@gmail.com>
2020-05-05 03:40:29 +0100

    Updates

---
+++ precache-manifest.0c6c0d5e976c0519b2613cee25647867.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afe2dfe683619528be9c567ef8f2946a",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "6f0161477068c8b56697",
+    "url": "/react-weather-app/static/css/main.d6e2fd58.chunk.css"
+  },
+  {
+    "revision": "d8344c6984acce31e1b2",
+    "url": "/react-weather-app/static/js/2.f5a4951c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.f5a4951c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f0161477068c8b56697",
+    "url": "/react-weather-app/static/js/main.c40fd88e.chunk.js"
+  },
+  {
+    "revision": "381d595b8f67db289ae3",
+    "url": "/react-weather-app/static/js/runtime-main.9485360c.js"
+  },
+  {
+    "revision": "f7abe8f53598ea90cf1fb35a60888167",
+    "url": "/react-weather-app/static/media/clear-sky.f7abe8f5.jpg"
+  },
+  {
+    "revision": "687b50e59c8d2184cb5c904412f0daea",
+    "url": "/react-weather-app/static/media/cloud.687b50e5.jpg"
+  },
+  {
+    "revision": "ed583e9f3911805657f413ad1b0de30f",
+    "url": "/react-weather-app/static/media/rainy1.ed583e9f.jpg"
+  },
+  {
+    "revision": "f8643ee9e19504622ab6a2d048ec4915",
+    "url": "/react-weather-app/static/media/warmer.f8643ee9.jpg"
+  },
+  {
+    "revision": "b6548e36fefabfea9cdb9d757d108cd7",
+    "url": "/react-weather-app/static/media/winter2.b6548e36.jpg"
+  }
+]);
